22ef3a50606f28643aedb87abea8d2d71d9c2c78
Aaron <a.rcottrill521@gmail.com>
2018-01-18 15:29:33 -0500

    Add project files.

---
+++ AaronCottrillSpyStore.DAL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AaronCottrillSpyStore.DAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
