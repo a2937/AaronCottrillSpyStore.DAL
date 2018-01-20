ceaf41f3c2d32b4d09dfd008e2a05f7c3493e581
Aaron <a.rcottrill521@gmail.com>
2018-01-20 12:00:26 -0500

    Get started on entity migration

---
+++ AaronCottrillSpyStore.DAL/EF/StoreContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AaronCottrillSpyStore.DAL.EF
+{
+    class StoreContext : DbContext
+    {
+        public StoreContext()
+        {
+
+        }
+
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SpyStore;Trusted_Connection=True;MultipleActiveResultSets=true;",options => options.ExecutionStrategy(c => new MyExecutionStrategy(c)));
+            }
+        }
+    }
+}
---
+++ AaronCottrillSpyStore.Models/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using AaronCottrillSpyStore.Models.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace AaronCottrillSpyStore.Models.Entities
+{
+    [Table("Categories",Schema = "Store")]
+    public class Category : EntityBase
+    {
+        [DataType(DataType.Text),MaxLength(50)]
+        public string CategoryName { get; set; }
+
+    }
+}
